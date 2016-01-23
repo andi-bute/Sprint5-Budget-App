99e421cd0a62828f3e948f1e50fd9b156c26dc02
panaiteandreisilviu <andrei_silviu.panaite@aut.pub.ro>
2016-01-23 10:30:11 +0200

    Initial setup

---
+++ js/abstractRepository.js
@@ -0,0 +1,87 @@
+"use strict";
+
+var url = 'server_url';
+var defaults = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+var abstractRepository = (function () {
+
+    var data = [];
+    return {
+        get: function (id) {
+            return new Promise(function (resolve, reject) {
+                $.ajax(url + '/' + id, defaults).done(resolve).error(function (xhr) {
+                    errorHandler(reject, xhr)
+                });
+            });
+        },
+        getAll: function (settings) {
+            return new Promise(function (resolve, reject) {
+
+                $.ajax(url, defaults).done(function (dataReceived) {
+                    resolve(dataReceived);
+                }).error(function (xhr) {
+                    errorHandler(reject, xhr)
+                });
+            });
+        },
+        add: function (item) {
+            return new Promise(function (resolve, reject) {
+                $.ajax(url + "/", $.extend({}, defaults, {
+                    type: "POST",
+                    data: JSON.stringify(item)
+                })).done(function (data) {
+                    resolve(data);
+                }).error(function (xhr) {
+                    errorHandler(reject, xhr)
+                })
+            });
+        },
+        update: function (id, updateData) {
+            return new Promise(function (resolve, reject) {
+
+                $.ajax(url + '/' + id, $.extend({}, defaults, {type: 'PUT', data: JSON.stringify(updateData)})
+                ).done(function (data) {
+                    resolve(data);
+                }).error(function (xhr) {
+                    errorHandler(reject, xhr);
+                });
+            });
+        },
+        delete: function (id) {
+            return new Promise(function (resolve, reject) {
+
+                $.ajax(url + '/' + id, {
+                    type: 'DELETE',
+                    headers: {'Content-Type': 'application/json'}
+                }).done(resolve).error(function (xhr) {
+                    errorHandler(reject, xhr)
+                })
+
+            });
+        }
+    };
+
+})();
+
+var errorHandler = function (reject, xhr) {
+    var errorCode = xhr.status;
+    var errorMessage = xhr.responseJSON.error;
+
+    if (errorCode == "409") {
+        reject(function () {
+            return errorMessage;
+        })
+    } else {
+        reject(function () {
+            return "Server Error";
+        })
+    }
+};
+
+function recurringRepository() {
+    var data = [{"id": 1, "catId": 1, "amount": 300, "day": 22, "tag": "Salary"}];
+}
